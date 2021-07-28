dbb07d9ab989608be685ba9634afddfa799a0948
Rogerio <12768403+rogix@users.noreply.github.com>
2021-07-28 19:37:49 -0300

    Encript password

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -2,6 +2,7 @@ import { ICreateUserDTO } from "../dtos/ICreatedUserDTO";
 
 interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<void>;
+  findByEmail(email: string): Promise<User>;
 }
 
 export { IUsersRepository };
